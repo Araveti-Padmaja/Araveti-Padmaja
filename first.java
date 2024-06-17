7f0b7e02ba7b205f0fb69e2b34e14b0292746984
Araveti-Padmaja <aravetipadmaja70@gmail.com>
2024-06-17 22:27:25 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,12 @@
+class sample{
+    public static void main(String args[])
+    {
+        int a, b, sum=0,product=0;
+        a=20;
+        b=90;
+        sum=a+b;
+        product=a*b;
+        System.out.println("sum="+sum);
+        System.out.println("product="+product);
+    }
+}
