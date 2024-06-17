cd6b0bdc6097a598254bd57bac36164a96538435
Araveti-Padmaja <aravetipadmaja70@gmail.com>
2024-06-17 22:27:05 +0530

    Add files via upload

---
+++ accept.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+class sample {
+    public static void main(String args[])
+    {
+        int a,b,sum=0,product=0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first no:");
+        a=scan.nextInt();
+        System.out.println("enter the second no:");
+        b=scan.nextInt();
+        sum=a+b;
+        product=a*b;
+        System.out.println("sum="+sum);
+        System.out.println("product="+product);
+
+    }
+}
+    
+
